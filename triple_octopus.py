084e463682426cd7e2134cec51620fb31473a3c6
Hope4orcas <124528587+Hope4orcas@users.noreply.github.com>
2023-02-14 17:48:49 +0100

    Update triple_octopus.py

--- triple_octopus.py
+++ triple_octopus.py
@@ -2,3 +2,18 @@ import streamlit as st
 
 texto=":violet[hellouda]"
 st.markdown(texto)
+
+asignaturas= ("Lengua","Inglés","Matemáticas","E.F","Física y Química")
+resultado=st.radio("¿Cual es tu asignatura favorita", asignaturas)
+
+st.write(resultado)
+if resultado==asignaturas[0]:
+  st.warning("No")
+if resultado==asignaturas[1]:
+  st.baloons()
+if resutado==asignaturas[2]:
+  st.success("Buena elección")
+if resultado==asignaturas[3]
+  st.error("incorrecto")
+if resultado==asignaturas[4]
+  st.success("Biennnnn")
